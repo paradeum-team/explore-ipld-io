f130e9e6eee00a252b5ded3989085bf0233d9a23
1071318859 <45553538+1071318859@users.noreply.github.com>
2023-03-31 11:13:19 +0800

    Update ipfs.js

--- src/bundles/ipfs.js
+++ src/bundles/ipfs.js
@@ -4,7 +4,7 @@ import { create } from 'ipfs-http-client'
 
 const defaultState = {
   apiOpts: {
-    host: '127.0.0.1',
+    host: 'test-ipfs-headless.test-ipfs-cluster',
     port: '5001',
     protocol: 'http'
   },
